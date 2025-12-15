521b7dc1919085304fa47d378594ebf5e7943e01
Dajarony Ysaac Guzmán Marmolejos <gataka534@gmail.com>
2025-12-15 18:50:04 +0000

    fix: Añadir manejo de errores global para prevenir crashes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'router/app_routes.dart';
@@ -6,7 +7,23 @@ import 'providers/theme_provider.dart';
 import 'providers/settings_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  // Capturar errores de Flutter
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.presentError(details);
+    debugPrint('Flutter Error: ${details.exception}');
+  };
+
+  // Ejecutar la app en zona segura para capturar errores async
+  runZonedGuarded(
+    () {
+      WidgetsFlutterBinding.ensureInitialized();
+      runApp(const MyApp());
+    },
+    (error, stackTrace) {
+      debugPrint('Uncaught error: $error');
+      debugPrint('Stack trace: $stackTrace');
+    },
+  );
 }
 
 class MyApp extends StatelessWidget {
