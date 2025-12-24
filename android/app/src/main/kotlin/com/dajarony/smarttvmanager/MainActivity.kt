0895742514603a524021f2d84b7aad71b45a406e
Dajarony Ysaac Guzmán Marmolejos <gataka534@gmail.com>
2025-12-24 17:33:39 +0000

    fix: Corregir MainActivity package, Gradle 8.11.1, JDK 17, AndroidX - App funciona correctamente

---
+++ android/app/src/main/kotlin/com/dajarony/smarttvmanager/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.dajarony.smarttvmanager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/mi_app_expriment2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mi_app_expriment2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
